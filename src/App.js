0904e9156b17aa0db3cddd746e19c5f813e2d006
Euniceadeola <euniceadejoke22@gmail.com>
2023-03-30 16:16:34 +0100

    final commt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Main from './main';
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <main></main>
+  );
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+const country = "Nigeria"
+const Main = () => {
+    return (
+<main>
+    <section>
+        <h2>About Us</h2>
+        <p>
+            We are a small team of web developers based in the united States. We Specialise in creating mordern and responsive website for businesses and individuals.
+        </p>
+    </section>
+    <section>
+        <h2>Our services</h2>
+        <ul>
+            <li>Web Design</li>
+            <li>Web Developments</li>
+            <li>E-commerce Solution</li>
+            <li>Search Engine Optimization (SEO)</li>
+        </ul>
+    </section>
+</main>
+
+    )
+}
+
+export default Main
